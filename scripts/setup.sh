8e2ea4da42536f432f58e93cb2542ad26999d3be
Lisha from Linux-Mint-MATE-OLDHP <lishastudies.cs@gmail.com>
2026-06-20 21:27:47 +0530

    Updated setup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Create a virtual environment named .venv
+set -e # exit immediately if error with non-zero return value
+
+# get into repo directory if not run from there
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REPO_ROOT="$(dirname "$SCRIPT_DIR")"
+
+cd "$REPO_ROOT"
+
+echo "Creating virtual environment..."
 python3 -m venv .venv
 
-# Activate the virtual environment
-source .venv/bin/activate
+echo "Upgrading pip..."
+.venv/bin/python -m pip install --upgrade pip
 
-# Install the dependencies listed in requirements.txt
-python -m pip install -r requirements.txt
+echo "Installing requirements..."
+if [ ! -f requirements.txt ]; then
+   echo
+   echo "ERROR: requirements.txt not found!"
+   exit 1
+fi
+
+.venv/bin/python -m pip install -r requirements.txt
+
+echo
+echo "Setup complete"
+echo
+echo "To activate venv in your terminal instance, run:"
+echo "   source .venv/bin/activate"
+echo
+echo "To run your python script, run:"
+echo "   python <your-file-name>.py"
